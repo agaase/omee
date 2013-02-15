ffab87a93db8c2c6cb22e2cae63e3c5974e19924
aseem <aseem@aseem-Inspiron-1420>
2013-02-16 00:50:31 +0530

    beta changes.

--- src/omee.js
+++ src/omee.js
@@ -121,3 +121,27 @@ omee.selectElement=function(id, value) {
 		}
 	}
 }
+
+
+/**
+ * Loads a css or js file dynamically to your page.
+ * @param filename- The path of the file.
+ * @param filetype- CSS/JS.
+ */
+omee.addcssjsfile = function(filename, filetype) {
+	if (filetype == "js") { // if filename is a external JavaScript file
+		var fileref = document.createElement('script');
+		fileref.setAttribute("type", "text/javascript");
+		fileref.setAttribute("src", filename);
+	} else if (filetype == "css") { // if filename is an external CSS file
+		var fileref = document.createElement("link");
+		fileref.setAttribute("rel", "stylesheet");
+		fileref.setAttribute("type", "text/css");
+		fileref.setAttribute("href", filename);
+	}
+	if (typeof fileref != "undefined")
+		document.getElementsByTagName("head")[0].appendChild(fileref);
+
+};
+
+
